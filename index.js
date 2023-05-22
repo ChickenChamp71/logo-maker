b3c326b584febb427c0cfb049d054897dcdce6d2
ChickenChamp71 <chickensandsharks@gmail.com>
2023-05-22 01:38:24 -0700

    finished inquirer test, added package for max input length

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const inquirer = require('inquirer');
+const maxLengthInquirer = require('inquirer-maxlength-input-prompt');
+
+inquirer.registerPrompt('maxlength-input', maxLengthInquirer);
 
 function test() {
     inquirer
         .prompt([
             {
+                type: 'maxlength-input',
+                name: 'text',
+                message: 'Choose three characters. (Maximum of three.)',
+                maxLength: 3
+            },
+            {
                 type: 'input',
-                name: 'test',
-                message: 'Testing testing?',
+                name: 'color',
+                message: 'Select a color. Can be a hex code or a simple color name.'
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose between these shapes:',
+                choices: ['Circle', 'Triangle', 'Square']
             }
         ])
+        .then((answers) => {
+
+            const data = answers;
+
+            console.log(data);
+        })
 }
 
 test();
