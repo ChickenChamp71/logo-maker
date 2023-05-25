294e82d5c4dd8074db06635c5ea6df9a9b7be1dc
ChickenChamp71 <chickensandsharks@gmail.com>
2023-05-25 01:20:15 -0700

    added shape creation and connected it to index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const inquirer = require('inquirer');
 const maxLengthInquirer = require('inquirer-maxlength-input-prompt');
+const fs = require('fs');
+const shapes = require('./lib/shapes');
 
 inquirer.registerPrompt('maxlength-input', maxLengthInquirer);
+
+function writeToFile(data) {
+
+    fs.writeFile('logo.svg', shapes(data), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Logo file successfully created! :D');
+        }
+    });
+}
 
 function test() {
     inquirer
@@ -14,21 +27,27 @@ function test() {
             },
             {
                 type: 'input',
-                name: 'color',
-                message: 'Select a color. Can be a hex code or a simple color name.'
+                name: 'textColor',
+                message: 'Select a color for the text. Can be a hex code or a simple color name.',
             },
             {
                 type: 'list',
                 name: 'shape',
                 message: 'Choose between these shapes:',
-                choices: ['Circle', 'Triangle', 'Square']
-            }
+                choices: ['Circle', 'Triangle', 'Square'],
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'Select a color for the shape. Can be a hex code or a simple color name.',
+            },
         ])
         .then((answers) => {
 
             const data = answers;
-
             console.log(data);
+            writeToFile(data);
+    
         })
 }
 
